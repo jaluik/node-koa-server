048981e562d39bc3d7f3f8e1ef6ad9739e327550
jaluik <cqzhengmouren@gmail.com>
2019-12-16 23:23:52 +0800

    引入mysql

--- config/config.js
+++ config/config.js
@@ -1,4 +1,11 @@
 module.exports = {
   // prod
-  enviroment: "dev"
+  enviroment: "dev",
+  database: {
+    dbName: "island",
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "123456"
+  }
 };
