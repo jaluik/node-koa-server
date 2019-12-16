94092e12ecd9291f11473453926672b1f45ce739
jaluik <cqzhengmouren@gmail.com>
2019-12-17 00:03:25 +0800

    更新连接

--- core/db.js
+++ core/db.js
@@ -14,7 +14,19 @@ const sequelize = new Sequelize(dbName, user, password, {
   port,
   logging: true,
   timezone: "+08:00",
-  define: {}
+  define: {
+    //   create_time , update_time
+    timestamps: true,
+    paranoid: true,
+    createAt: "create_at",
+    updateAt: "update_at",
+    deleteAt: "delete_at",
+    underscored: true
+  }
+});
+
+sequelize.sync({
+  force: true
 });
 
 module.exports = {
