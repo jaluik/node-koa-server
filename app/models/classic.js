4183344f86e58b3522a42d236112bfd55860b9e8
jaluik <cqzhengmouren@gmail.com>
2019-12-18 23:02:34 +0800

    更新

---
+++ app/models/classic.js
@@ -0,0 +1,38 @@
+const { sequelize } = require("../../core/db");
+const { Sequelize, Model } = require("sequelize");
+
+const classicFields = {
+  image: Sequelize.STRING,
+  content: Sequelize.STRING,
+  pubdate: Sequelize.DATEONLY,
+  fav_nums: Sequelize.INTEGER,
+  title: Sequelize.STRING,
+  type: Sequelize.TINYINT
+};
+
+class Movie extends Model {}
+Movie.init(classicFields, {
+  sequelize,
+  tableName: "movie"
+});
+
+class Sentence extends Model {}
+Sentence.init(classicFields, {
+  sequelize,
+  tableName: "movie"
+});
+
+class Music extends Model {}
+Music.init(
+  { ...classicFields, url: Sequelize.STRING },
+  {
+    sequelize,
+    tableName: "movie"
+  }
+);
+
+module.exports = {
+  Movie,
+  Sentence,
+  Music
+};
