c9744b2f44d6db6984e28fc06618eccb6cdbe6d2
jaluik <cqzhengmouren@gmail.com>
2019-12-19 23:27:59 +0800

    新增flow、classic

--- app/api/v1/classic.js
+++ app/api/v1/classic.js
@@ -1,10 +1,17 @@
 const Router = require("koa-router");
-const router = new Router();
+const router = new Router({
+  prefix: "/v1/classic"
+});
+const { Flow } = require("../../models/flow");
 
-router.get("/v1/classic/latest", (ctx, next) => {
-  ctx.body = {
-    key: "classic"
-  };
+const { PositiveIntegerValidator } = require("../../validators/validator");
+const { Auth } = require("../../../middlewares/auth");
+
+router.get("/latest", new Auth().m, async (ctx, next) => {
+  const flow = await Flow.findOne({
+    order: [["index", "DESC"]]
+  });
+  ctx.body = flow;
 });
 
 module.exports = router;
